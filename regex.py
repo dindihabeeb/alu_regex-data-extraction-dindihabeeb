e6b6d384bcf88b54a349ec66cb810d2a9a1bf3b6
Habeeb Dindi <habeebullahdindi@gmail.com>
2025-05-19 21:59:23 +0200

    Fix pattern value, prepend with r

--- regex.py
+++ regex.py
@@ -1,33 +1,33 @@
 import re
 
-def match_email(email):
+def check_email(email):
     pattern = r"^[a-zA-Z.]+@[a-zA-Z.]+\.[a-zA-Z]{2,}$"
     return re.match(pattern, email) is not None
 
-def pattern_link(link):
-    pattern = "^(https?):\/\/([a-zA-Z0-9.-]+)(\/[\S]*)?$"
+def check_link(link):
+    pattern = r"^(https?):\/\/([a-zA-Z0-9.-]+)(\/[\S]*)?$"
     return re.match(pattern, link) is not None
     
-def pattern_card(card):
-    pattern = "^(\d{4}[ -]){3}\d{4}$"
+def check_card(card):
+    pattern = r"^(\d{4}[ -]){3}\d{4}$"
     return re.match(pattern, card) is not None
 
-def pattern_phone(phone):
-    pattern = "^(\(?\d{3}\)?)[-. ](\d{3})[-.](\d{4})$"
+def check_phone(phone):
+    pattern = r"^(\(?\d{3}\)?)[-. ](\d{3})[-.](\d{4})$"
     return re.match(pattern, phone) is not None
     
-def pattern_time(time):
-    pattern = "^\d{1,2}:\d{1,2}( [AP]M)?$"
+def check_time(time):
+    pattern = r"^\d{1,2}:\d{1,2}( [AP]M)?$"
     return re.match(pattern, time) is not None
 
-def pattern_html(tag):
-    pattern = "^<\/?[a-zA-Z][a-zA-Z0-9-]*\b[^>]*>$"
-    return re.match(pattern, time) is not None
+def check_html(tag):
+    pattern = r"^<\/?[a-zA-Z][a-zA-Z0-9-]*\b[^>]*>$"
+    return re.match(pattern, tag) is not None
 
-def pattern_hashtag(hashtag):
-    pattern = "^#[a-zA-Z0-9]+$"
+def check_hashtag(hashtag):
+    pattern = r"^#[a-zA-Z0-9]+$"
     return re.match(pattern, hashtag) is not None
 
-def pattern_amount(amount):
-    pattern = "^\$\d{1,3}(,\d{3})*\.\d{2}$"
+def check_amount(amount):
+    pattern = r"^\$\d{1,3}(,\d{3})*\.\d{2}$"
     return re.match(pattern, amount) is not None
